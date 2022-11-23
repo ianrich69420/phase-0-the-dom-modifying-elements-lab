5f24898af43c45fe72a7842abdc551fc8981a0c7
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-11-23 21:26:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+const newHeaderText = document.createTextNode("Ian is the champion")
+newHeader.appendChild(newHeaderText)
